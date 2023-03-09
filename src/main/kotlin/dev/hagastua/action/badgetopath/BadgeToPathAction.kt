b9d6872d5d4d1e4606e93dbcbd28222418983d1a
Jan-Petter Krüger <73356377+janpetterkruger@users.noreply.github.com>
2023-03-09 21:01:12 +0100

    Implemented simple badge generation

--- src/main/kotlin/dev/hagastua/action/badgetopath/BadgeToPathAction.kt
+++ src/main/kotlin/dev/hagastua/action/badgetopath/BadgeToPathAction.kt
@@ -1,10 +1,27 @@
 package dev.hagastua.action.badgetopath
 
+import io.github.dsibilio.badgemaker.core.BadgeFormatBuilder
+import io.github.dsibilio.badgemaker.core.BadgeMaker
+import io.github.dsibilio.badgemaker.model.NamedColor
 import io.quarkiverse.githubaction.Action
+import io.quarkiverse.githubaction.Inputs
+import java.io.File
 
-class BadgeToPathAction {
+open class BadgeToPathAction {
   @Action
-  fun test() {
-    println("Hello from BadgeToPath GitHub Action")
+  fun action(inputs: Inputs) {
+
+    val label = inputs.getRequired("label")
+    val status = inputs.getRequired("status")
+    val path = inputs.getRequired("path")
+
+    val badge =
+        BadgeMaker.makeBadge(
+            BadgeFormatBuilder(status)
+                .withLabel(label)
+                .withLabelColor(NamedColor.BLUE)
+                .withMessageColor(NamedColor.GREY)
+                .build())
+    File(path).writeText(badge)
   }
 }
