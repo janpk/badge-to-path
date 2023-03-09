4098e226b9d2b57098b938452cd68a226d84074d
Jan-Petter Krüger <73356377+janpetterkruger@users.noreply.github.com>
2023-03-09 13:59:58 +0100

    Initial commit

---
+++ src/main/kotlin/dev/hagastua/action/badgetopath/BadgeToPathAction.kt
@@ -0,0 +1,10 @@
+package dev.hagastua.action.badgetopath
+
+import io.quarkiverse.githubaction.Action
+
+class BadgeToPathAction {
+  @Action
+  fun test() {
+    println("Hello from BadgeToPath GitHub Action")
+  }
+}
